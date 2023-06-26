64fd080342f993e04426c579c10ad89c7bf0b875
Alici96 <dangelo.alicia2.7@gmail.com>
2023-06-26 13:10:07 +0200

    Beliebig viele Breischluckbilder visualisierbar (davor: nur bis zu 3)

--- 3drekonstruktionspeiseroehre/gui/more_files.py
+++ 3drekonstruktionspeiseroehre/gui/more_files.py
@@ -15,14 +15,12 @@ class ShowMoreWindows(QMainWindow):
         self.master_window: MasterWindow = master_window
 
         w_list = []
-        #next_window = None
 
         # erzeuge alle Fenster aller Eingabedaten und speicher diese
         for n, visualization in enumerate(visualization_list):
             # visualization ist None, wenn kein Bild an der Stelle n eingegeben wurde
             if visualization is not None:
                 xray_selection_window = XrayRegionSelectionWindow(self.master_window, visualization, n)
-                #xray_selection_window.next_window = next_window
                 w_list.append(xray_selection_window)
             else:
                 w_list.append(None)
@@ -30,78 +28,21 @@ class ShowMoreWindows(QMainWindow):
         # setze für jedes Fenster das nächste Fenster
         for i, w in enumerate(w_list):
             # falls das erste Fenster, dann initialisiere all_visualization mit leerer Liste
-            if i == 0 and w is not None: # erstes Bild ist not None
-                # erstes Fenster ist auch letztes Fenster
-                if w_list[1] is None and w_list[2] is None:
-                    next_window = None
-                    w.all_visualization = []
-                # es gibt ein erstes Fenster und ein zweites Fenster → nächstes Fenster erzeugen
-                elif w_list[1] is not None:
-                    next_window = w_list[1]
-                    w.all_visualization = []
-                # es gibt das erste und das dritte Bild, aber nicht das zweite
-                elif w_list[2] is not None and w_list[1] is None:
-                    next_window = w_list[2]
-                    w.all_visualization = []
-                w.next_window = next_window
-            # elif i == 0 and w is None: # erstes Bild ist None
-            #     # es gibt kein weiteres Bild wird nicht überprüft, da Annahme, dass mind 1 geladen wird
-            #     if w_list[1] is not None: # zweites Bild ist nicht None
-            #         next_window = w_list[1]
-            #         w.all_visualization = []
-            #     # zweites Bild nicht geladen, drittes schon
-            #     elif w_list[1] is None and w_list[2] is not None:
-            #         next_window = w_list[2]
-            #         w.all_visualization = []
-            # zweites Fenster
-            if i == 1 and w is not None:
-                # zweites Fenster ist auch letztes Fenster, es gibt kein drittes Bild
-                if w_list[2] is None:
-                    next_window = None
-                    # wenn es das erste Bild gab:
-                    if w_list[0] is not None:
-                        w.all_visualization = w_list[0].all_visualization
-                    else:
-                        w.all_visualization = []
-                # es gibt ein zweites Fenster und ein drittes Fenster → nächstes Fenster erzeugen
-                elif w_list[2] is not None:
-                    next_window = w_list[2]
-                    # wenn es das erste Bild gab:
-                    if w_list[0] is not None:
-                        w.all_visualization = w_list[0].all_visualization
-                    else:
-                        w.all_visualization = []
-                w.next_window = next_window
-            # drittes Fenster
-            if i == 2 and w is not None:
+            if i == 0:
+                next_window = w_list[i+1]
+                w.all_visualization = []
+            # falls letztes Fenster, dann setze kein nächstes Fenster
+            elif i == len(w_list)-1:
                 next_window = None
-                if w_list[1] is not None:
-                    w.all_visualization = w_list[1].all_visualization
-                elif w_list[0] is not None:
-                    w.all_visualization = w_list[0].all_visualization
-                else:
-                    w.all_visualization = []
-                w.next_window = next_window
+                w.all_visualization = w_list[i-1].all_visualization
+            # falls in der Mitte, nächstes Fenster erzeugen
+            # hole Daten (all_visualisation) vom vorherigen Fenster
+            else:
+                next_window = w_list[i+1]
+            w.all_visualization = w_list[i - 1].all_visualization
+            w.next_window = next_window
 
-        # not_none = (e for e in w_list if e is not None)
-        # value = next(not_none, None)
-        # if value is not None:
-        if w_list[0] is not None:
-            self.master_window.switch_to(w_list[0])
-        elif w_list[1] is not None:
-            self.master_window.switch_to((w_list[1]))
-        elif w_list[2] is not None:
-            self.master_window.switch_to((w_list[2]))
-        elif w_list[3] is not None:
-            self.master_window.switch_to((w_list[3]))
-        elif w_list[4] is not None:
-            self.master_window.switch_to((w_list[4]))
-        elif w_list[5] is not None:
-            self.master_window.switch_to((w_list[5]))
-        elif w_list[6] is not None:
-            self.master_window.switch_to((w_list[6]))
-        else:
-            print("is None")
+        self.master_window.switch_to(w_list[0])
 
 
 
