slope rotation, corrected perpendiculars #21

3drekonstruktionspeiseroehre/logic/figure_creator/figure_creator.py
@@ -7,6 +7,9 @@ import plotly.express as px
 import shapely.geometry
 from logic.visualization_data import VisualizationData
 from skimage import graph
+from sklearn.linear_model import LinearRegression
+from scipy import spatial
+import matplotlib.pyplot as plt
 
 
 class FigureCreator(ABC):
@@ -177,63 +180,126 @@ class FigureCreator(ABC):
         """
         widths = []
         centers = []
+        slopes = []
         offset_top = sensor_path[0][0] # y-value of first point in path
 
+        num_points_for_polyfit = 60
+        eps = 0.01
+
+        # Create a figure and axis FOR DEBUGGING
+        fig, ax = plt.subplots()
+
+        # Invert y-axis to have positive y downwards FOR DEBUGGING
+        ax.invert_yaxis()
+        # plot xray FOR DEBUGGING
+        coordinates = [(row_idx, col_idx) for row_idx, row in enumerate(visualization_data.xray_mask) for col_idx, value in enumerate(row) if value == 1]
+        x_values, y_values = zip(*coordinates)
+        ax.scatter(y_values, x_values)
+
+        # plot shortest path FOR DEBUGGING
+        x_values = [point[1] for point in sensor_path] # in sensor path stehen die x werte an index 1
+        y_values = [point[0] for point in sensor_path]
+        ax.plot(x_values,y_values, color="red")
+        plt.savefig("path.png")
+
         for i in range(len(sensor_path)-1):
-            current_point = sensor_path[i]
-            next_point = sensor_path[i+1]
-
-            # Calculate Perpendicular 
-            if (next_point[0] - current_point[0]) != 0:
-                m = (next_point[1] - current_point[1]) / (next_point[0] - current_point[0])
-                if m != 0:
-                    perpendicular_slope = -1 / m
-                else:
-                    perpendicular_slope = -1 / 0.0001
+            # Create slope_points that are used to calculate linear regression (slope)
+            if i < num_points_for_polyfit //2:
+                # Before we have num_points_for_polyfit point available skip to the part where we have enough to calcuate the slope
+                point = sensor_path[num_points_for_polyfit//2]
+                slope_points = sensor_path[0 : num_points_for_polyfit-1]
+            elif i + num_points_for_polyfit // 2 > len(sensor_path) - 1:
+                # At the end we don't have enough points to calculate the slope, use the last point where it was possible
+                point = sensor_path[len(sensor_path) - 1 - num_points_for_polyfit // 2]
+                slope_points = sensor_path[len(sensor_path) - num_points_for_polyfit : len(sensor_path) - 1]
             else:
-                perpendicular_slope = 0
+                # Get surrounding points
+                point = sensor_path[i]
+                slope_points = sensor_path[i-num_points_for_polyfit//2 : i+num_points_for_polyfit//2 - 1]
+            
+            # x and y coords of slope_points
+            x = np.array([p[1] for p in slope_points]).reshape(-1, 1)
+            # Edit x so x-values aren't the same (sklearn can't handle that)
+            x = np.array([x+ i * 0.00001 for i, x in enumerate(x)])
+            y = np.array([p[0] for p in slope_points])
+            # Calculate linear regression to get slope of esophagus segment
+            model = LinearRegression()
+            model.fit(x,y)
+            slopes.append(model.coef_[0]) # TODO: oder perpendicular slope
 
-            # Calculate the two points for the perpendicular line
-            mid_point = (current_point[0] + next_point[0]) / 2, (current_point[1] + next_point[1]) / 2
-            # Calculate the step size for generating points
-            step_size = 200 / 2  # Divide by 2 since you're calculating points on both sides
-            # Generate 200 points along the perpendicular line
-            perpendicular_x_values = np.linspace(mid_point[0] - step_size, mid_point[0] + step_size, num=200)
-            perpendicular_y_values = perpendicular_slope * (perpendicular_x_values - mid_point[0]) + mid_point[1]
+            if model.coef_[0] < 0:
+                eps = -eps
 
+            # Calculate perpendicular slope, use epsilon to avoid divisions by zero or values close to zero
+            perpendicular_slope = -1 / model.coef_[0]
+
+            line_length = visualization_data.xray_mask.shape[1]
+            # Calculate equidistant points between two points on perpendicular (equidistant to avoid skipping points later)
+            # new_y                        y     + m               * (new_x - x)
+            perpendicular_start_y=  point[0] + perpendicular_slope * (0 - point[1]) # TODO: fix 
+            perpendicular_end_y = point[0] + perpendicular_slope * (visualization_data.xray_mask.shape[1] - 1 - point[1])
+            perpendicular_start = (perpendicular_start_y, 0)
+            perpendicular_end = (perpendicular_end_y, visualization_data.xray_mask.shape[1]-1)
+
+            if model.coef_[0] > 1000 or model.coef_[0] < -1000:
+                # If the points used for the lin reg are inline along the y axis (slope is very high/steep)
+                perpendicular_start = (point[0] , point[1] - line_length)
+                perpendicular_end = (point[0], point[1] + line_length)
+
+            y1,x1 = int(perpendicular_start[0]), int(perpendicular_start[1])
+            y2,x2 = int(perpendicular_end[0]), int(perpendicular_end[1])
+            num_points = max(abs(x2 - x1), abs(y2 - y1)) + 1
+            perpendicular_x_values = np.linspace(x1, x2, num_points, dtype=int)
+            perpendicular_y_values = np.linspace(y1, y2, num_points, dtype=int)
             perpendicular_points = [(int(y), int(x)) for y, x in zip(perpendicular_y_values, perpendicular_x_values)]
 
-            boundary_points = []  # Store points where the line intersects with boundaries
-    
-            # Iterate over points between point1 and point2
-            for point_along_line in perpendicular_points:
-                # Check that point is within image
-                if  point_along_line[0]>0 and point_along_line[0] < visualization_data.xray_mask.shape[0] and point_along_line[1]<visualization_data.xray_mask.shape[1]:
-                    if visualization_data.xray_mask[point_along_line[0]][point_along_line[1]] == 1:
-                        boundary_points.append(point_along_line)
-            
-            # Check if there are at least 2 boundary points
-            if len(boundary_points) < 2:
-                continue
+            # Find index of current point / its closest equal in perpendicular
+            _, index = spatial.KDTree(np.array(perpendicular_points)).query(np.array(point))
+        
+            boundary_1 = None
+            boundary_2 = None
+        
+            # Move left and right from the current point along the perpendicular to find the boundaries
+            for j in range(len(perpendicular_points) // 2 - 5):
+                # Move "left" until boundary is found
+                if boundary_1 is None:
+                    point_along_line = perpendicular_points[index-j]
+                    # Check that point is within image
+                    if point_along_line[0] >= 0 and point_along_line[1] >= 0 and point_along_line[0] < visualization_data.xray_mask.shape[0] and point_along_line[1]<visualization_data.xray_mask.shape[1]:
+                        if visualization_data.xray_mask[point_along_line[0]][point_along_line[1]] == 0 and boundary_1 is None:
+                            boundary_1 = perpendicular_points[index-j + 1]
+                    
+                # Move "right" until boundary is found
+                if boundary_2 is None:
+                    point_along_line = perpendicular_points[index+j]
+                    # Check that point is within image
+                    if point_along_line[0] >= 0 and point_along_line[1] >= 0 and point_along_line[0] < visualization_data.xray_mask.shape[0] and point_along_line[1]<visualization_data.xray_mask.shape[1]:
+                        if visualization_data.xray_mask[point_along_line[0]][point_along_line[1]] == 0:
+                            boundary_2 = perpendicular_points[index+j -1]
 
+            # FOR DEBUGGING
+            if boundary_1 is not None and boundary_2 is not None:
+                plt.plot([boundary_1[1], boundary_2[1]],[boundary_1[0], boundary_2[0]], color="red")
+            
+            # Check if there are at least 2 boundary points 
+            if boundary_1 is None or boundary_2 is None:
+                plt.plot(perpendicular_x_values,perpendicular_y_values, color="green") # FOR DEBUGGING
+                plt.savefig("perp_points.png")
+                break
             # Step 2: Calculate Width
             # Calculate the distance between two boundary points
-            width = np.linalg.norm(np.array(boundary_points[0]) - np.array(boundary_points[-1]))
+            width = np.linalg.norm(np.array(boundary_1) - np.array(boundary_2))
             
             # Step 3: Calculate Center
             # Calculate the midpoint between two boundary points
-            center = (np.array(boundary_points[0]) + np.array(boundary_points[-1])) / 2
+            center = (np.array(boundary_1) + np.array(boundary_2)) / 2
+            center = (int(center[0]), int(center[1]))
             
             # Store the calculated width and center
             widths.append(width)
             centers.append(center)
-
-        # Calculate slope angles for every center
-        slopes = []
-        for i in range(len(centers)-1):
-            current_center = centers[i]
-            next_center = centers[i+1]
-            slopes.append((next_center[0] - current_center[0]) / (next_center[1] - current_center[1]))
+        plt.savefig("perp_points.png") # FOR DEBUGGING
+            
         return widths, centers, slopes, offset_top
 
     @staticmethod
@@ -286,14 +352,14 @@ class FigureCreator(ABC):
         for i in range(visualization_data.xray_mask.shape[0]):
             # Iterate over xray mask width horizontally (x-axis)
             for j in range(visualization_data.xray_mask.shape[1]):
-                if visualization_data.xray_mask[i, j] == True:
+                if visualization_data.xray_mask[i, j]:
                     if not start_top:
-                        start_top = (i,j)
-                    end_top = (i,j)
+                        start_top = (i, j)
+                    end_top = (i, j)
                     found_top = True
             if found_top:
-                startpoint = (end_top[0], (end_top[1]-start_top[1])/2)
-                break        
+                startpoint = (start_top[0], (start_top[1] + end_top[1]) // 2)
+                break      
 
         # Use annotated endpoint as end of shortest path
         endpoint = visualization_data.esophagus_exit_pos

3drekonstruktionspeiseroehre/logic/figure_creator/figure_creator_without_endoscopy.py
@@ -50,17 +50,19 @@ class FigureCreatorWithoutEndoscopy(FigureCreator):
         y = np.array(y)
         z = np.array(z)
 
-        # # Apply rotation matrix
-        # for i in range(len(slopes)):
-        #     slope_in_rad = atan(slopes[i])
-        #     # Rotate around y-axis according to slopes
-        #     rotated_coordinates = np.matmul(
-        #         np.array([[np.cos(slope_in_rad), 0, np.sin(slope_in_rad)],
-        #                 [0, 1, 0],
-        #                 [-np.sin(slope_in_rad), 0, np.cos(slope_in_rad)]]), np.array([x[i], [0]* len(x[i]), z[i]]))
+        # Apply rotation matrix
+        for i in range(len(z)):
+            slope_in_rad = atan(slopes[i])
+            # Rotate around y-axis according to slopes
+            rotated_coordinates = np.matmul(
+                np.array([[np.cos(slope_in_rad), 0, np.sin(slope_in_rad)],
+                        [0, 1, 0],
+                        [-np.sin(slope_in_rad), 0, np.cos(slope_in_rad)]]), np.array([x[i]-centers[i][1], y[i], z[i]-i]))
             
-        #     # Rotated x and z coordinates
-        #     x[i], _, z[i] = rotated_coordinates
+            # Rotated x and z coordinates
+            x[i], _, z[i] = rotated_coordinates
+            x[i] += centers[i][1]
+            z[i] += i
 
         # Shift axes to start at zero and scale to cm
         px_to_cm_factor = esophagus_full_length_cm / esophagus_full_length_px