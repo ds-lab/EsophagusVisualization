fb0616f97870aa9e9f1d6263ced0368037f11788
schnepia <pia.schneider@med.uni-augsburg.de>
2023-11-29 15:41:46 +0100

    Eingabecheck bei Previous-Therapies

--- 3drekonstruktionspeiseroehre/gui/previous_therapies_window.py
+++ 3drekonstruktionspeiseroehre/gui/previous_therapies_window.py
@@ -15,3 +15,24 @@ class PreviousTherapiesWindow(QMainWindow):
         self.ui = uic.loadUi("./ui-files/previous_therapies_window_design.ui", self)
         self.master_window: MasterWindow = master_window
         self.patient_data: PatientData = patient_data
+        self.ui.save_button.clicked.connect(self.__save_button_clicked)
+
+
+    def __save_button_clicked(self):
+        """
+        checks data in previous therapies and saves them
+        """
+        if (not self.ui.botox_check.isChecked() and not self.ui.pneu_dil_30_check.isChecked() and not
+            self.ui.pneu_dil_35_check.isChecked() and not self.ui.pneu_dil_40_check.isChecked() and not
+            self.ui.poem_check.isChecked() and not self.ui.lapro_check.isChecked() and not
+            self.ui.other_check.isChecked()
+        ):
+            print("eine Therapie auswählen")
+        elif (self.ui.botox_check.isChecked() and self.ui.botox_spin.value() == 0 or
+              self.ui.pneu_dil_30_check.isChecked() and self.ui.botox_spin.value() == 0 or
+              self.ui.pneu_dil_35_check.isChecked() and self.ui.pneu_dil_35_spin.value() == 0 or
+              self.ui.pneu_dil_40_check.isChecked() and self.ui.pneu_dil_40_spin.value() == 0 or
+              self.ui.poem_check.isChecked() and self.ui.poem_spin.value() == 0 or
+              self.ui.lapro_check.isChecked() and self.ui.lapro_spin.value() == 0 or
+              self.ui.other_check.isChecked() and self.ui.other.spin.value() == 0):
+            print("Bitte Eingaben prüfen")
