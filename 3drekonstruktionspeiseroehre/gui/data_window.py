de3559d5d37d034bcbcb87915c18ae514a6fff1c
schnepia <pia.maria.schneider@student.uni-augsburg.de>
2024-03-28 12:45:52 +0100

    Manometry Daten werden hübscher angezeigt (erstes Attribut wird weggelassen)

--- 3drekonstruktionspeiseroehre/gui/data_window.py
+++ 3drekonstruktionspeiseroehre/gui/data_window.py
@@ -782,7 +782,11 @@ class DataWindow(QMainWindow):
         if manometry is not None:
             attributes = vars(manometry)
             text = ""
+            first_attribute_skipped = False
             for attribute, value in attributes.items():
+                if not first_attribute_skipped:
+                    first_attribute_skipped = True
+                    continue
                 text += f"{attribute}: {value}\n"
             self.ui.manometry_text.setText(text)
         else:
