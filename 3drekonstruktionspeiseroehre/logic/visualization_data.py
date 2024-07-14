6a40a5c58dfe61221ce3aabf2df32c0980051b3d
schnepia <pia.maria.schneider@student.uni-augsburg.de>
2024-07-14 15:39:43 +0200

    Bug Fix Laden der Xray-Bilder für Position-Selection Window von DB statt von File

--- 3drekonstruktionspeiseroehre/logic/visualization_data.py
+++ 3drekonstruktionspeiseroehre/logic/visualization_data.py
@@ -6,10 +6,10 @@ class VisualizationData:
         init VisualizationData
         """
         self._xray_filename = None
+        self._xray_minute = None
         self._xray_file = None
 
         self._endoscopy_files = None
-        self._endoscopy_filenames = None
 
         self._xray_polygon = None
         self._xray_image_height = None
@@ -53,6 +53,14 @@ class VisualizationData:
     @xray_filename.setter
     def xray_filename(self, value):
         self._xray_filename = value
+
+    @property
+    def xray_minute(self):
+        return self._xray_minute
+
+    @xray_minute.setter
+    def xray_minute(self, value):
+        self._xray_minute = value
 
     @property
     def xray_polygon(self):
@@ -109,14 +117,6 @@ class VisualizationData:
     @endoscopy_files.setter
     def endoscopy_files(self, value):
         self._endoscopy_files = value
-
-    @property
-    def endoscopy_filenames(self):
-        return self._endoscopy_filenames
-
-    @endoscopy_filenames.setter
-    def endoscopy_filenames(self, value):
-        self._endoscopy_filenames = value
 
     @property
     def endoscopy_image_positions_cm(self):
