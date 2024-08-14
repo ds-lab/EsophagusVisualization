a0f4275156edb097fd4be1c146251d3290cdbb91
jochneda <david.jochner@student.uni-augsburg.de>
2024-08-14 20:16:54 +0200

    Branch erstellt

--- 3drekonstruktionspeiseroehre/main.py
+++ 3drekonstruktionspeiseroehre/main.py
@@ -5,7 +5,7 @@ from logic.database.database import create_db_and_tables_local_declarative
 from PyQt6.QtWidgets import QApplication
 from gui.data_window import DataWindow
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     # create the MasterWindow and show the first UI
 
@@ -18,6 +18,7 @@ if __name__ == '__main__':
     try:
         # close the splash screen if running as pyinstaller-exe
         import pyi_splash
+
         pyi_splash.close()
     except ModuleNotFoundError:
         pass
