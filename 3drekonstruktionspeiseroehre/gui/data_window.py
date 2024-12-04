fa3273630a31a8268b6c3341afca31685f79ec26
schnepia <pia.maria.schneider@student.uni-augsburg.de>
2024-12-04 12:54:55 +0100

    Felder mit Anzeige der Daten werden nochmal aktualisiert nachdem Files (bei Manometry und Endoflip) hochgeladen wurden + Start des Upload jetzt immer vom vorherigen Pfad

--- 3drekonstruktionspeiseroehre/gui/data_window.py
+++ 3drekonstruktionspeiseroehre/gui/data_window.py
@@ -933,6 +933,9 @@ class DataWindow(QMainWindow):
             if len(filename) > 0:
                 process_and_upload_manometry_file(self.selected_visit, filename)
                 self.ui.manometry_file_text.setText("Manometry File uploaded")
+                self.__init_manometry()
+
+            self.default_path = os.path.dirname(filename)
 
     def __add_barium_swallow(self):
         tbe = self.barium_swallow_service.get_barium_swallow_for_visit(self.selected_visit)
@@ -1004,6 +1007,8 @@ class DataWindow(QMainWindow):
                     self.barium_swallow_image_index = 0
                     self.barium_swallow_minutes = barium_swallow_minutes
                     self.__load_barium_swallow_image()
+
+            self.default_path = os.path.dirname(filename)
 
     def __load_barium_swallow_image(self):
         # Load and display the current image
@@ -1090,6 +1095,8 @@ class DataWindow(QMainWindow):
                     self.endoscopy_image_index = 0
                     self.endoscopy_positions = endoscopy_positions
                     self.__load_endoscopy_image()
+
+            self.default_path = os.path.dirname(filename)
 
     def __load_endoscopy_image(self):
         # Load and display the current image
@@ -1190,6 +1197,7 @@ class DataWindow(QMainWindow):
                             conduct_endoflip_file_upload(self.selected_visit, timepoint, data_bytes,
                                                          endoflip_screenshot)
                 self.ui.endoflip_file_text.setText(str(len(filenames)) + " File(s) uploaded")
+                self.__init_endoflip()
             self.default_path = os.path.dirname(filename)
 
     def __upload_endoflip_image(self):
@@ -1220,6 +1228,9 @@ class DataWindow(QMainWindow):
                     self.endoflip_image_index = 0
                     self.endoflip_timepoints = endoflip_timepoints
                     self.__load_endoflip_image()
+
+            self.default_path = os.path.dirname(filename)
+
 
     def __load_endoflip_image(self):
         # Load and display the current image
@@ -1283,6 +1294,8 @@ class DataWindow(QMainWindow):
                     self.endosono_positions = endosono_positions
                     self.__load_endosonography_image()
 
+            self.default_path = os.path.dirname(filename)
+
     def __load_endosonography_image(self):
         # Load and display the current image
         if 0 <= self.endosono_image_index < len(self.endosono_pixmaps):
@@ -1316,6 +1329,8 @@ class DataWindow(QMainWindow):
                 self.endosonography_video_service.save_video_for_visit(visit_id=self.selected_visit,
                                                                        video_file_path=filename)
             self.ui.endosono_videos_text.setText(str(len(filenames)) + " Videos(s) uploaded")
+
+            self.default_path = os.path.dirname(filename)
 
     def __download_endosonography_video(self):
         destination_directory = QFileDialog.getExistingDirectory(self, "Select Directory")
